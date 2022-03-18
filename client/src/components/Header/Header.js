d048e3e740a2e95239ddbcc26f2341e8c60bffd2
imjord <dean.jay.goat@gmail.com>
2022-03-18 13:08:05 -0600

    idk styles

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './style.css';
+
+
+const Header = () => {
+    return (
+        <div className="idk">
+<div className="header-container">
+            La Luna Bros
+        </div>
+        </div>
+        
+    )
+}
+
+export default Header;
---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './style.css';
+
+
+const Posts = () => {
+    return (
+        <div>Posts</div>
+    )
+}
+
+export default Posts;
